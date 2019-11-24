23b81db33c5ad8746f9035c413e8e2fdc794a0f8
zied <zied.hack@gmail.com>
2019-11-24 14:31:53 +0100

    fix Servlet

---
+++ tp1/src/monocontrol/Verification2.java
@@ -0,0 +1,50 @@
+package monocontrol;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Verification2
+ */
+public class Verification2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Verification2() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		PrintWriter out=rs.getWriter();
+		String login=r.getParameter("login");
+		if (login.equals("admin"))
+				{
+				out.println ("<b> Bonjour Mr "+ r.getParameter("nom")+r.getParameter("prenom")+"vous êtes bien l'administrateur</b>");}
+		else {
+			
+			out.print("ce n'est pas l'administrateur");
+		}
+
+		
+	}
+
+}
